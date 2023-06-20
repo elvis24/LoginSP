459083d110b3c80c2be1dabcd4c4e9e921deb3ca
elvis24 <villaca6@hotmail.com>
2023-06-19 23:45:27 -0500

    Agregar archivos de proyecto.

---
+++ LoginSP/Controllers/AccesoController.cs
@@ -0,0 +1,113 @@
+﻿using LoginSP.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+
+
+
+namespace LoginSP.Controllers
+
+{
+    public class AccesoController : Controller
+    {
+        static string cadena = "Data Source=.;Initial Catalog=DB_ACCESO;Integrated Security=true";
+
+        // GET: Acceso
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Usuario oUsuario)
+        {
+            bool resgistrado;
+            string mensaje;
+
+            if (oUsuario.Clave == oUsuario.ConfirmarClave)
+            {
+                oUsuario.Clave = ConvertirSha256(oUsuario.Clave);
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Las contraseñas no coinciden";
+                return View();
+            }
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("sp_ResgistrarUsuario", cn);
+                cmd.Parameters.AddWithValue("Correo",oUsuario.Correo);
+                cmd.Parameters.AddWithValue("Clave", oUsuario.Clave);
+                cmd.Parameters.Add("Registrado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("Mensaje", SqlDbType.VarChar,100).Direction = ParameterDirection.Output;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                resgistrado = Convert.ToBoolean(cmd.Parameters["Registrado"].Value);
+                mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+            }
+            ViewData["Mensaje"] = "Las contraseñas no coinciden";
+            if (resgistrado)
+            {
+                return RedirectToAction("Login", "Acceso");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Login(Usuario usuario)
+        {
+            usuario.Clave = ConvertirSha256(usuario.Clave);
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("sp_ValidarUsuario", cn);
+                cmd.Parameters.AddWithValue("Correo", usuario.Correo);
+                cmd.Parameters.AddWithValue("Clave", usuario.Clave);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+                usuario.IdUsuario=Convert.ToInt32(cmd.ExecuteScalar().ToString());
+
+            }
+
+            if (usuario.IdUsuario != 0)
+            {
+                Session["usuario"] = usuario;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewData["Mensaje"] = "Usuario no encontrado";
+                return View();
+            }
+        }
+
+        public static string ConvertirSha256(string texto)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding e = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(e.GetBytes(texto));
+                foreach (byte b in result)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
